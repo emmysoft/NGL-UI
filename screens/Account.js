44a7a8bdb31ac5f4f6971efe2af5f55cd921cdff
emmysoft <emmanueloni45@gmail.com>
2024-01-02 08:48:40 -0800

    adding profile page template

--- screens/Account.js
+++ screens/Account.js
@@ -1,12 +1,15 @@
+//import libraries
 import { View, Text } from 'react-native'
 import React from 'react'
+import tw from 'twrnc';
 
 const Account = () => {
-  return (
-    <View>
-      <Text>Account</Text>
-    </View>
-  )
+    return (
+        <View style={tw`flex flex-row justify-center items-center gap-4 bg-white h-full`}>
+            <Text style={tw`bg-[#0000ff] text-white px-5 py-5 rounded-lg`}>Anonymous</Text>
+            <Text style={tw`bg-[#ef2673] text-white px-5 py-5 rounded-lg`}>Confession</Text>
+        </View>
+    )
 }
 
 export default Account;
